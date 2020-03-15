0cacdad23a500d68ea6962a21cab7ba0491df174
admin_syscontrol <info.igrover@gmail.com>
2020-03-14 23:11:02 -0400

    gzip publisher

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"github.com/pulkitmehra/go-recipies/pkg/simple"
-	"github.com/pulkitmehra/go-recipies/pkg/web"
+	"github.com/pulkitmehra/go-recipies/pkg/inout"
 )
 
 func main() {
 
-	simple.FnMain()
-	web.MainWeb()
+	//simple.FnMain()
+	inout.GzipMain()
 
-	/* simple.CopyArrayAndSlice([]int{1, 2, 3, 4}, []int{5, 6, 7, 8})
-	simple.SortMain()
-	simple.PointerMain()
-	simple.StringMain() */
+	/*
+		web.MainWeb()
+		simple.CopyArrayAndSlice([]int{1, 2, 3, 4}, []int{5, 6, 7, 8})
+		simple.SortMain()
+		simple.PointerMain()
+		simple.StringMain() */
 }
--- pkg/web/server.go
+++ pkg/web/server.go
@@ -2,19 +2,42 @@ package web
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/valyala/fasthttp"
 )
 
 //MainWeb method for orchestrating
 func MainWeb() {
+	wrapperMain()
+	callServer()
+}
+
+func callServer() {
 	startServer()
+	defer call("http://localhost:5678/close")
+	fmt.Println(call("http://localhost:5678/test"))
+
+}
+
+func call(url string) string {
+	r, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
 }
 
 func startServer() {
 	done := make(chan bool)
 	go func() {
-		fasthttp.ListenAndServe(":8081", func(ctx *fasthttp.RequestCtx) {
+		fasthttp.ListenAndServe(":5678", func(ctx *fasthttp.RequestCtx) {
 			switch string(ctx.Path()) {
 			case "/test":
 				ctx.SetBody([]byte(`recieved!`))
@@ -25,6 +48,6 @@ func startServer() {
 		})
 
 	}()
-	fmt.Println("to kill call http://localhost:8081/close")
+	fmt.Println("to kill call http://localhost:5678/close")
 	<-done
 }
